d7124a0efa9cbc787e824ad599f62d57bc525645
joeggg <ninjoetsu@gmail.com>
2021-09-05 14:09:52 +0100

    Added commands to change google tts settings + better error handling from backend

--- app/backend.js
+++ app/backend.js
@@ -5,6 +5,12 @@ const zmq = require('zeromq');
 
 const logger = require('./util/logger');
 
+const NS_IN_S = 1000000000;
+const say_config_commands = {'voice': null, 'pitch': null, 'rate': null};
+
+/**
+ * Basic ZMQ REQ interface with async wrapper to make API calls easily
+ */
 class ZMQRouter {
     constructor() {
         this.sck = zmq.socket('req');
@@ -12,7 +18,7 @@ class ZMQRouter {
         this.sck.connect(address);
         logger.logInfo(`Backend socket connected at ${address}`);
         this.response = null;
-        this.maxWait = 30*1000000000;
+        this.maxWait = 30*NS_IN_S;
         this.sck.on('message', (res)=> {
             this.response = res;
         });
@@ -50,26 +56,54 @@ function setupRouter() {
     router = new ZMQRouter();
 }
 
-async function say_test(text) {
+/**
+ * Google texttospeech API access via Python backend
+ *  2 commands available:
+ *    - Request a voice clip of some text (args all input text) 
+ *    - Changes the current voice used, args are ['voice', '<voicename>']
+ * @param {string[]} args 
+ */
+async function say_test(args) {
     if (!router) setupRouter();
 
-    const req = {
-        command: 'say_test',
-        params: {
-            text: text.join(' ')
-        }
-    };
-    const response = await router.make_call(req);
-    if (response.result === 'success') {
-        const file = new discord.MessageAttachment(nconf.get('texttospeech_dir'));
-        return {content: '', files: [file]};
+    if (args.length === 0) return 'No text received';
 
+    const req = {command: null, params: {}};
+    let say = true;
+    if (args[0] in say_config_commands) {
+        say = false;
+        req.command = `change_google_${args[0]}`;
+        req.params[args[0]] = args[1];
+    } else {
+        req.command = 'say_test';
+        req.params.text = args.join(' ');
+    }
+
+    const response = await router.make_call(req);
+    if (response.code === 0) {
+        if (say) {
+            const file = new discord.MessageAttachment(nconf.get('texttospeech_dir'));
+            return {content: '', files: [file]};
+        }
+    } 
+    if (response.result) {
+        return response.result;
     }
     throw new Error('Backend failure');
 }
 
+/**
+ * Simulates a set of dice rolls for DND,
+ *  Args are in the format ['<numRolls><diceType>', ...]
+ *  e.g: ['1d4', '4d8', '2d20']
+ *  Format checking is done backend-side
+ * 
+ * @param {string[]} args 
+ */
 async function dice(args){
     if (!router) setupRouter();
+
+    if (args.length === 0) return 'No arguments';
 
     const req = {
         command: 'dnd_dice_roll',
@@ -78,16 +112,19 @@ async function dice(args){
         }
     };
     const response = await router.make_call(req);
-    if (response.result instanceof Object) {
-        let message = '';
-        for (const [dice, res] of Object.entries(response.result)) {
-            message += `${dice}: [${res}]\n`;
+    if (response.code === 0) {
+        if (response.result instanceof Object) {
+            let message = '';
+            for (const [dice, res] of Object.entries(response.result)) {
+                message += `${dice}: [${res}]\n`;
+            }
+            return message;
         }
-        return message;
-    } else if (response.result === 'failure') {
-        throw new Error('Backend failure');
     }
-    throw new Error('Could not parse results');
+    if (response.result) {
+        return response.result;
+    }
+    throw new Error('Backend failure');
 }
 
 let router = null;
