fbb5e0da1a1b52d44673d39e4fd3559ab3232b97
joeggg <ninjoetsu@gmail.com>
2021-10-31 20:46:56 +0000

    Better queue displaying and direct url search added

--- app/music.js
+++ app/music.js
@@ -10,80 +10,83 @@ let subscription = null;
 
 
 async function handleYt(args, msg) {
-    const command = args[0];
+	const command = args[0];
 	const query = args.slice(1).join(' ');
 
-    if (command === 'play') {
-        return await play(query, msg);
-    } else if (command === 'queue') {
-        return await queue(query);
-    } else if (command === 'pause') {
-        return await pause();
-    } else if (command === 'resume') {
-        return await resume();
-    } else if (command === 'skip') {
-        return await skip();
-    } else if (command === 'quit') {
+	if (command === 'play') {
+		return await play(query, msg);
+	} else if (command === 'queue') {
+		return await queue(query);
+	} else if (command === 'pause') {
+		return await pause();
+	} else if (command === 'resume') {
+		return await resume();
+	} else if (command === 'skip') {
+		return await skip();
+	} else if (command === 'quit') {
 		return await quit();
 	} else if (command === 'show') {
 		return await show();
 	} else {
-        return 'Invalid yt command';
-    }
+		return 'Invalid yt command';
+	}
 }
 
 async function play(query, msg) {
-    if (!msg.member.voice.channel) {
-        return 'You aren\'t in a voice channel';
-    }
+	if (!msg.member.voice.channel) {
+		return 'You aren\'t in a voice channel';
+	}
 	if (!query) {
 		return 'No query given';
 	}
 
-    if (!subscription) {
-        const channel = msg.member.voice.channel;
+	if (!subscription) {
+		const channel = msg.member.voice.channel;
 
-        subscription = new MusicSubscription(
-            discordVoice.joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
+		subscription = new MusicSubscription(
+			discordVoice.joinVoiceChannel({
+				channelId: channel.id,
+				guildId: channel.guild.id,
 				adapterCreator: channel.guild.voiceAdapterCreator,
-            })
-        );
+			})
+		);
 
-        // Make sure the connection is ready before processing the user's request
+		// Make sure the connection is ready before processing the user's request
 		try {
 			await discordVoice.entersState(
-                subscription.voiceConnection,
-                discordVoice.VoiceConnectionStatus.Ready,
-                20000
-            );
+				subscription.voiceConnection,
+				discordVoice.VoiceConnectionStatus.Ready,
+				20000
+			);
 		} catch (error) {
 			logger.logError(error);
 			return 'Failed to join voice channel within 20 seconds, please try again later!';
 		}
-    }
+	}
 
-    try {
+	try {
 		subscription.audioPlayer.stop(true);
-        return await queue(query);
-    } catch (error) {
-        logger.logError(error);
-        return 'Failed to play track, please try again later!';
-    }
+		return await queue(query);
+	} catch (error) {
+		logger.logError(error);
+		return 'Failed to play track, please try again later!';
+	}
 }
 
 async function queue(query) {
 	// Search for the query on Youtube and return the first video's URL
 	const url = await search(query);
-    // Attempt to create a Track from the video URL
-    const track = await Track.from(url);
-    // Enqueue the track and reply a success message to the user
-    subscription.enqueue(track);
-    return `Queued [${track.title}]`;
+	// Attempt to create a Track from the video URL
+	const track = await Track.from(url);
+	// Enqueue the track and reply a success message to the user
+	subscription.enqueue(track);
+	return `Queued [${track.title}]`;
 }
 
 async function search(query) {
+	if (query.includes('youtube.com/watch?v=')) {
+		return query;
+	}
 	const auth = new google.auth.GoogleAuth({
 		keyFile: 'token/google_key.json',
 		scopes: ['https://www.googleapis.com/auth/youtube.readonly'],
@@ -103,7 +106,7 @@ async function search(query) {
 				res(resp.data.items);
 			}
 		});
-	}); 
+	});
 	const url = `https://youtube.com/watch?v=${data[0].id.videoId}`;
 	return url;
 }
@@ -115,22 +118,22 @@ async function skip() {
 	if (subscription.queue.length === 0) {
 		return 'No tracks left';
 	}
-    subscription.audioPlayer.stop(true);
-    return 'Skipped a song';
+	subscription.audioPlayer.stop(true);
+	return 'Skipped a song';
 }
 
 async function pause() {
 	if (!subscription) {
 		return 'No music playing';
 	}
-    subscription.audioPlayer.pause();
+	subscription.audioPlayer.pause();
 }
 
 async function resume() {
 	if (!subscription) {
 		return 'No music playing';
 	}
-    subscription.audioPlayer.unpause();
+	subscription.audioPlayer.unpause();
 }
 
 async function quit() {
@@ -142,14 +145,15 @@ async function quit() {
 }
 
 async function show() {
-    if (subscription && subscription.queue.length > 0) {
-        let output = '';
-        for (const track of subscription.queue) {
-            output += `[${track.title}]\n`;
-        }
-        return output;
-    }
-    return 'No queued tracks to show';
+	if (subscription && subscription.currentTrack) {
+		let output = '  Current queue:\n';
+		output += `| ->${subscription.currentTrack.title}\n`;
+		for (const track of subscription.queue) {
+			output += `|   ${track.title}\n`;
+		}
+		return output;
+	}
+	return 'No queued tracks to show';
 }
 
 
@@ -181,7 +185,7 @@ class Track {
 			const stream = process.stdout;
 			const onError = error => {
 				if (!process.killed) process.kill();
-                logger.logError(error);
+				logger.logError(error);
 				stream.resume();
 				reject(error);
 			};
@@ -189,11 +193,11 @@ class Track {
 				.once('spawn', () => {
 					discordVoice.demuxProbe(stream)
 						.then(probe => resolve(
-                            discordVoice.createAudioResource(
-                                probe.stream,
-                                { metadata: this, inputType: probe.type }
-                            )
-                        ))
+							discordVoice.createAudioResource(
+								probe.stream,
+								{ metadata: this, inputType: probe.type }
+							)
+						))
 						.catch(onError);
 				})
 				.catch(onError);
@@ -207,7 +211,7 @@ class Track {
 	 * @param methods Lifecycle callbacks
 	 * @returns The created Track
 	 */
-	 static async from(url) {
+	static async from(url) {
 		const info = await getInfo(url);
 		return new Track({
 			title: info.videoDetails.title,
@@ -226,15 +230,17 @@ class MusicSubscription {
 		this.voiceConnection = voiceConnection;
 		this.audioPlayer = discordVoice.createAudioPlayer();
 		this.queue = [];
+		this.currentTrack = null;
 
 		// Configure audio player
-		this.audioPlayer.on ('stateChange', async (oldState, newState) => {
+		this.audioPlayer.on('stateChange', async (oldState, newState) => {
 			if (
-                newState.status === discordVoice.AudioPlayerStatus.Idle &&
-                oldState.status !== discordVoice.AudioPlayerStatus.Idle
-            ) {
+				newState.status === discordVoice.AudioPlayerStatus.Idle &&
+				oldState.status !== discordVoice.AudioPlayerStatus.Idle
+			) {
 				// If the Idle state is entered from a non-Idle state, it means that an audio resource has finished playing.
 				// The queue is then processed to start playing the next track, if one is available.
+				this.currentTrack = null;
 				this.processQueue();
 			}
 		});
@@ -253,15 +259,15 @@ class MusicSubscription {
 		this.processQueue();
 	}
 
-    quit() {
-        this.queueLock = true;
-        this.audioPlayer.stop(true);
-        this.voiceConnection.disconnect();
-    }
+	quit() {
+		this.queueLock = true;
+		this.audioPlayer.stop(true);
+		this.voiceConnection.disconnect();
+	}
 
-    clearQueue() {
-        this.queue = [];
-    }
+	clearQueue() {
+		this.queue = [];
+	}
 
 	/**
 	 * Attempts to play a Track from the queue
@@ -269,10 +275,10 @@ class MusicSubscription {
 	async processQueue() {
 		// If the queue is locked (already being processed), is empty, or the audio player is already playing something, return
 		if (
-            this.queueLock ||
-            this.audioPlayer.state.status !== discordVoice.AudioPlayerStatus.Idle ||
-            this.queue.length === 0
-        ) {
+			this.queueLock ||
+			this.audioPlayer.state.status !== discordVoice.AudioPlayerStatus.Idle ||
+			this.queue.length === 0
+		) {
 			return;
 		}
 		// Lock the queue to guarantee safe access
@@ -280,6 +286,7 @@ class MusicSubscription {
 
 		// Take the first item from the queue. This is guaranteed to exist due to the non-empty check above.
 		const nextTrack = this.queue.shift();
+		this.currentTrack = nextTrack;
 		try {
 			logger.logInfo('Playing track');
 			// Attempt to convert the Track into an AudioResource (i.e. start streaming the video)
@@ -297,5 +304,5 @@ class MusicSubscription {
 
 
 module.exports = {
-    handleYt: handleYt,
+	handleYt: handleYt,
 };
